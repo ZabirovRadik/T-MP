54a53dd6ab503453dacb3d1d92f70ac6bf611295
Radik Zabirov <zabirovradik070@gmail.com>
2024-09-14 18:05:04 +0400

    Fixed a bug in the source code

--- lab_1/yatzy3.py
+++ lab_1/yatzy3.py
@@ -15,6 +15,7 @@ class Yatzy:
 
     @staticmethod
     def yatzy(d1: int, d2: int, d3: int, d4: int, d5: int) -> int:
+        dice = [d1, d2, d3, d4, d5]
         counts = [0]*(len(dice)+1)
         for die in dice:
             counts[die-1] += 1
